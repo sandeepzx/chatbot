724d101a08bbf2d60a675184eacaee7051d7530d
Sandeep A S <sandeepshine865@gmail.com>
2026-02-18 11:11:26 +0530

    remembering previous chats

--- app.py
+++ app.py
@@ -5,13 +5,16 @@ from dotenv import load_dotenv
 from langchain_groq import ChatGroq
 from langchain_core.prompts import ChatPromptTemplate
 
+# Get credentials from env file
 load_dotenv()
 groq_api_key = os.getenv("GROQ_API_KEY")
 database_url = os.getenv("DATABASE_URL")
 
+# Make connection to database
 def get_db_connection():
     return psycopg2.connect(database_url)
 
+# Save to database
 def save_to_db(user_id, role, message):
     try:
         conn = get_db_connection()
@@ -29,9 +32,11 @@ def save_to_db(user_id, role, message):
     except Exception as e:
         print(f"Error saving to database: {e}")
 
+# Prompt
 prompt = ChatPromptTemplate.from_messages(
     [
         ("system", "you are a diet specialist, give me the output accordingly"),
+        ("placeholder", "{history}"),
         ("user", "{question}")
     ]
 )  
@@ -40,13 +45,32 @@ chain = prompt | llm
 
 user_id = "User123"
 
+# Get previous chats
+def get_history(user_id):
+    conn = get_db_connection()
+    cur = conn.cursor()
+    
+    cur.execute("SELECT role, message FROM chat WHERE user_id = %s ORDER BY timestamp", (user_id,))
+    chats = cur.fetchall() 
+
+    history = []
+    for chat in chats:
+        history.append((chat[0],chat[1]))
+    
+    cur.close()
+    conn.close()
+
+    return history
+
+# Enter the chat
 while True:
     question = input("Enter a question: ")
 
     if question.lower() in ["exit","quit"]:
         break
 
-    response = chain.invoke({"question": question})
+    history = get_history(user_id)
+    response = chain.invoke({"history": history,"question": question})
 
     save_to_db(user_id, "user", question)
     save_to_db(user_id, "assistant", response.content)

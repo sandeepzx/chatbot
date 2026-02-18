9833d9489613c0321624de9027594b73f916ab1f
Sandeep A S <sandeepshine865@gmail.com>
2026-02-18 09:49:21 +0530

    add prompt

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import os
 from dotenv import load_dotenv
 from langchain_groq import ChatGroq
-
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 groq_api_key = os.getenv("GROQ_API_KEY")
 
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", "you are a diet specialist, give me the output accordingly"),
+        ("user", "{question}")
+    ]
+)  
 llm = ChatGroq(api_key=groq_api_key, model="openai/gpt-oss-20b")
+chain = prompt | llm
 
-response = llm.invoke("how to do pullups?")
+response = chain.invoke({"question": "how to do pullups?"})
 print(response.content)
